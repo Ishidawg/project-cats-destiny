8d8b015f79f0a8f8c98e93ed006b594a9f354753
Ishidawg <willianscagol@gmail.com>
2025-11-25 17:21:56 -0300

    fix: post cutscene scene

--- scenes/dialogue/dialogue_player.gd
+++ scenes/dialogue/dialogue_player.gd
@@ -7,13 +7,11 @@ var selected_text := []
 var in_progress := false
 
 @onready var background := $DialogTexture
-@onready var npc_name := $DialogTexture/DialogVerticalContainer/DialogName
-@onready var dialogue_text := $DialogTexture/DialogVerticalContainer/RichTextLabel
-@onready var dialog_character := $DialogCharacter
+@onready var npc_name := $DialogTexture/DialogName
+@onready var dialogue_text := $DialogTexture/RichTextLabel
 
 @export var dialog_key := ""
 @export var title_key := ""
-@export_file("*") var character_image
 
 signal open_dialogue
 signal close_dialogue
@@ -25,7 +23,7 @@ func _ready() -> void:
 	
 func _input(event):
 	if event.is_action_pressed("accept"):
-		SignalDisplayDialogue.emit_signal("display_dialog", title_key, dialog_key, character_image)
+		SignalDisplayDialogue.emit_signal("display_dialog", title_key, dialog_key)
 
 func load_scene_text() -> Dictionary:
 	if FileAccess.file_exists(scene_text_file):
@@ -48,24 +46,21 @@ func finish() -> void:
 	set_dialogue_false()
 	close_dialogue.emit()
 
-func on_display_dialog(title_key, text_key, character_image) -> void:
+func on_display_dialog(title_key, text_key) -> void:
 	if in_progress:
 		next_line()
 	else:
 		set_dialogue_true()
-		dialog_character.texture = load(character_image)
 		npc_name.text = scene_text[title_key] # As the title is only one and doe not change, we can just pass the value from the json that IS NOT an array
 		selected_text = scene_text[text_key].duplicate()
 		show_dialogue()
 
 func set_dialogue_false() -> void:
 	background.visible = false
-	dialog_character.visible = false
 	in_progress = false
 	npc_name.text = ""
 	dialogue_text.text = ""
 	
 func set_dialogue_true() -> void:
 	background.visible = true
-	dialog_character.visible = true
 	in_progress = true
